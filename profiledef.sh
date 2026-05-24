f9f8e02422bf7be5c87422a526af8ae994e6adae
cocosilex <cocosilex@gmail.com>
2026-05-24 21:51:12 +0200

    add base scripts to install arch <3

--- profiledef.sh
+++ profiledef.sh
@@ -20,6 +20,9 @@ file_permissions=(
   ["/root/.automated_script.sh"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
-  ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/usr/local/bin/mp2i-install"]="0:0:755"
+  ["/usr/local/bin/mp2i-partition"]="0:0:755"
+  ["/usr/local/bin/mp2i-base"]="0:0:755"
+  ["/usr/local/bin/mp2i-chroot"]="0:0:755"
 )
